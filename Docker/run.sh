36154efc987fba68845ea5f7f017c2c74b5db9b1
N-Eiki <naganagaeichan@gmail.com>
2023-04-03 10:31:17 +0900

    modify

--- Docker/run.sh
+++ Docker/run.sh
@@ -2,8 +2,7 @@
 xhost +local:root
 
 docker run --gpus all --rm -it --net host --ipc host --privileged\
+    -v /home/nagata/sources/fetch/src/fetch_ros:/root/catkin_ws/src/fetch_ros\
     -v /home/nagata/sources/fetch/src/fetch_test/:/root/catkin_ws/src/fetch_test\
     -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY \
-    fetch:latest bash
-
-        # -v /home/nagata/sources/fetch/src/fetch_ros:/root/catkin_ws/src/fetch_ros\
+    fetch:melodic bash
