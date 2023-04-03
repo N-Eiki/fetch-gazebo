b6e1c44625064a852b064b41be156f9f8cfc8ed4
N-Eiki <naganagaeichan@gmail.com>
2023-04-03 09:27:21 +0900

    modify path

--- Docker/run.sh
+++ Docker/run.sh
@@ -3,5 +3,6 @@ xhost +local:root
 
 docker run --gpus all --rm -it --net host --ipc host --privileged\
     -v /home/nagata/sources/fetch/src/fetch_ros:/root/catkin_ws/src\
+    -v /home/nagata/sources/fetch/src/fetch_ros/fetch_test:/root/catkin_ws/src/fetch_test\
     -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY \
     fetch:melodic bash
